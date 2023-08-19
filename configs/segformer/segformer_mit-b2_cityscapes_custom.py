d2a9ff04f53a637ca77b0b2d9235bac099089feb
Siddharth-Shrivastava7 <siddharthmanik16@gmail.com>
2023-08-19 13:07:48 +0530

    update

--- configs/segformer/segformer_mit-b2_cityscapes_custom.py
+++ configs/segformer/segformer_mit-b2_cityscapes_custom.py
@@ -14,14 +14,9 @@ test_pipeline = [
     dict(
         type='MultiScaleFlipAug',
         img_scale=(2048, 1024),
-        # img_ratios=[0.5, 0.75, 1.0, 1.25, 1.5, 1.75],
         flip=False,
         transforms=[
-            dict(type='Resize', keep_ratio=True),
             dict(type='CityTransform'),
-            # dict(type='RandomFlip'),
-            # dict(type='Normalize', **img_norm_cfg),
-            # dict(type='ImageToTensor', keys=['img']),
             dict(type='Collect', keys=['img']),
         ])
 ]
