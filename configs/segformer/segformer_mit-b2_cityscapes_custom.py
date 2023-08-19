400729f3e9a6004f9f6af87477521dfe97924e35
Siddharth-Shrivastava7 <siddharthmanik16@gmail.com>
2023-08-19 12:43:38 +0530

    update

---
+++ configs/segformer/segformer_mit-b2_cityscapes_custom.py
@@ -0,0 +1,30 @@
+_base_ = ['./segformer_mit-b0_8x1_1024x1024_160k_cityscapes.py']
+
+checkpoint = 'https://download.openmmlab.com/mmsegmentation/v0.5/pretrain/segformer/mit_b2_20220624-66e8bf70.pth'  # noqa
+model = dict(
+    backbone=dict(
+        init_cfg=dict(type='Pretrained', checkpoint=checkpoint),
+        embed_dims=64,
+        num_layers=[3, 4, 6, 3]),
+    decode_head=dict(in_channels=[64, 128, 320, 512]))
+
+
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(2048, 1024),
+        # img_ratios=[0.5, 0.75, 1.0, 1.25, 1.5, 1.75],
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='CityTransform'),
+            # dict(type='RandomFlip'),
+            # dict(type='Normalize', **img_norm_cfg),
+            # dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img']),
+        ])
+]
+data = dict(
+    val=dict(pipeline=test_pipeline),
+    test=dict(pipeline=test_pipeline))
