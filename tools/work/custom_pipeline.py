02a03cddf87c3896ab10d8cbfb5bbc7fc6bd9229
Siddharth-Shrivastava7 <siddharthmanik16@gmail.com>
2023-08-19 13:35:43 +0530

    fixed custom data transforms to give original results

--- tools/work/custom_pipeline.py
+++ tools/work/custom_pipeline.py
@@ -19,7 +19,6 @@ class CityTransform:
         
         input = Image.open(os.path.join(results['filename'])) 
         data_transforms = transforms.Compose([
-            transforms.RandomHorizontalFlip(),
             transforms.ToTensor(),
             transforms.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
         ])
